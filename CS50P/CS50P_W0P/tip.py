4c276cd6e10556dae1681eb68c934f95e9b84506
Aremeer <theartemeer@gmail.com>
2023-09-28 20:01:51 +0200

    1

--- CS50P/CS50P_W0P/tip.py
+++ CS50P/CS50P_W0P/tip.py
@@ -1,19 +1,23 @@
+import string
+
+punctuations = string.punctuation 
+
 def main():
-    dollars = dollars_to_float(input("How much was the meal? ").strip("$").lower())
-    percent = percent_to_float(input("What percentage would you like to tip? ").strip("%").lower())
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
     tip = dollars * percent
     print(f"Leave ${tip:.2f}")
 
 
-def dollars_to_float(x):
-    i = float(x)
-    return i
+def dollars_to_float(txt):
+    strippedLowerText = txt.strip(punctuations).lower()
+    return(float(strippedLowerText))
 
 
 
-def percent_to_float(x):
-    i = float(x)/100
-    return i
+def percent_to_float(txt):
+    strippedLowerText = txt.strip(punctuations).lower()
+    return(float(strippedLowerText)/100)
 
 
 main()
