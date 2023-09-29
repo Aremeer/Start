9a405ba3f641839533f42bbdc3e5719aeaef9618
Aremeer <theartemeer@gmail.com>
2023-09-29 08:21:09 +0200

    1

---
+++ CS50P/CS50P_W3P/taqueria.py
@@ -0,0 +1,24 @@
+menu = {
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+def main():
+    #prompt user for an item, one per line
+    #until the user inputs control-d
+    #after print price of all inpuded items "f${total}"
+
+
+
+
+
+
+
+
+main()
