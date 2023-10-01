98ae9f7bdb43a7199f798dac77e9379a14190d33
Aremeer <theartemeer@gmail.com>
2023-10-01 09:19:17 +0200

    1

--- CS50P/CS50P_W3P/outdated.py
+++ CS50P/CS50P_W3P/outdated.py
@@ -1,4 +1,6 @@
-#https://stackabuse.com/how-to-split-string-on-multiple-delimiters-in-python/
+#https://bobbyhadz.com/blog/python-input-date
+#https://stackoverflow.com/questions/69806492/regex-d4-d2-d2
+
 months = [
     "January",
     "February",
@@ -15,23 +17,35 @@ months = [
 ]
 
 import re
-
+from datetime import date
+#prompt user for a date mm/dd/yyyy or month dd, yyyy
+#output date in YYYY-MM-DD format
+#if usre's input is not valid in either format, prompt again
+#assume that month has no more than 31 days.
 def main():
-    date = getWrongDate()
-    print(date)
-    
-def getWrongDate():
     while True:
-        date = input("Date: ").lower().title()
-        numberPattern = r'^\d{1,2}/\d{1,2}/\d{4}$'
-        txtPattern = r"[a-zA-Z]+\s\d{1,2},\s\d{4}"
-        if re.match(numberPattern, date):
-            return date
-        if re.match(txtPattern, date):
-            return date
-        else:
+        try:
+            dateComponents = promptForDate()
+            dateChecked = checkIfAplpha(dateComponents)
+            month, day, year = [int(item) for item in dateChecked]
+        except ValueError:
             break
-        
-#def fixWrongDate(wrongDate):
+        print(f"{year}-{month:02}-{day:02}")
+        break
+def promptForDate():
+    dateComponents = input("Date: ").lower().title()
+    table = dateComponents.maketrans(",/", "  ")
+    dateComponents = dateComponents.translate(table)
+    dateComponents = dateComponents.split()
+    return dateComponents
+
+def checkIfAplpha(d):
+    if d[0] in months:
+        month = months.index(d[0])+1
+        d.remove(d[0])
+        d.insert(0, month)
+        return d
+    else:
+        return d
 
 main()
