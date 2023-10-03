2bf6effa32c456589d15a4ac9d40e5805c050ea6
Aremeer <theartemeer@gmail.com>
2023-10-03 21:28:49 +0200

    1

--- CS50P/CS50P_W4P/professor.py
+++ CS50P/CS50P_W4P/professor.py
@@ -3,30 +3,19 @@ import sys
 
 def main():
     level = get_level()
-    totalWrongAnswers = 0
+    totalRightAnswers = int("0")
     for _ in range(10):
         number1 = generate_integer(level)
         number2 = generate_integer(level)
         answer = sum([number1, number2])
-        for _ in range(3):
-            try: 
-                userInput = int(input(f"{number1} + {number2} = "))
-            except ValueError:
-                print("EEE")
-                continue
-            if userInput == answer:
-                break
-            if userInput != answer:
-                print("EEE")
-                continue
-        else:
-            print(f"{number1} + {number2} = {answer}")
-            totalWrongAnswers = totalWrongAnswers + 1
-            
-    print(f"Score: {10 - totalWrongAnswers}")
-    sys.exit(0)
-        
-        
+        userInput = input(f"{number1} + {number2} = ")
+        totalRightAnswers = checkUserAnswer(userInput, answer, number1, number2, totalRightAnswers)
+    if totalRightAnswers == None:
+        print ("Score: 0")
+    else:
+        print("Score:", totalRightAnswers)
+    exit(0)
+    
 def get_level():
     while True:
             level = input("Level: ")
@@ -49,6 +38,25 @@ def generate_integer(level):
     else:
         number = random.randrange(100, 999)
         return number
+
+
+def checkUserAnswer(userInput, answer, number1, number2, totalRightAnswers):
+    for _ in range(2):
+        try:
+            userInput = int(userInput)
+        except ValueError:
+            print("EEE")
+            userInput = input(f"{number1} + {number2} = ")
+            continue
+        if userInput == answer:
+            totalRightAnswers = totalRightAnswers + 1
+            return totalRightAnswers
+        if userInput != answer:
+            print("EEE")
+            userInput = input(f"{number1} + {number2} = ")
+            continue
+    print(f"{number1} + {number2} = {answer}")
+    print()
 
 
 if __name__ == "__main__":
