4eda903bf6c0004374aa33c49c37a5e0348cacd8
Aremeer <theartemeer@gmail.com>
2023-10-05 01:30:05 +0200

    1

--- CS50P/Week 5 problems/test_fuel/fuel.py
+++ CS50P/Week 5 problems/test_fuel/fuel.py
@@ -3,10 +3,16 @@ def main():
     #percentage = convert(fraction)
 
 def convert(fraction):
-    fraction = int(fraction.split("/"))
-    x = fraction[0]
-    y = fraction[1]
-    return x, y
+    fraction = fraction.split("/")
+    if fraction[0].isalpha() or fraction[1].isalpha():
+        raise ValueError("Inputs must be integer")
+    
+    x = int(fraction[0])
+    y = int(fraction[1])
+    if x > y:
+        raise ValueError
+    percentage = round(x/y)
+    return percentage
 
 
 #def gauge(percentage):
@@ -15,7 +21,8 @@ def convert(fraction):
 if __name__ == "__main__":
     main()
 #"""convert expects a str in X/Y format as input, wherein each of X and Y is an integer, and returns that fraction as a percentage rounded to the nearest int between 0 and 100, inclusive. If X and/or Y is not an integer, or if X is greater than Y, then convert should raise a ValueError. If Y is 0, then convert should raise a ZeroDivisionError.
+
 #gauge expects an int and returns a str that is:
-#"E" if that int is less than or equal to 1,
-#"F" if that int is greater than or equal to 99,
-#and "Z%" otherwise, wherein Z is that same int."""
+    #"E" if that int is less than or equal to 1,
+    #"F" if that int is greater than or equal to 99,
+    #and "Z%" otherwise, wherein Z is that same int."""
--- CS50P/Week 5 problems/test_fuel/test_fuel.py
+++ CS50P/Week 5 problems/test_fuel/test_fuel.py
@@ -1,5 +1,14 @@
 from fuel import convert
+import pytest
 
 def test_input():
-    assert convert("2/2") == 2, 2
-    assert convert("1/a") == 1, "a"
+    assert convert("2/2") == 1
+    assert convert("100/2") == 50
+    
+def test_input_alpha():
+    with pytest.raises(ValueError) as execinfo:
+        convert("a/2")
+    assert str(execinfo.value) == "Inputs must be integer" 
+    with pytest.raises(ValueError) as execinfo:
+        convert("a/2")
+    assert str(execinfo.value) == "Inputs must be integer" 
