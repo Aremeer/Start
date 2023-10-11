6bfeb9d64e33c6dceaac382fe9cc0054a12f08c6
Aremeer <theartemeer@gmail.com>
2023-10-11 04:17:38 +0200

    1

--- CS50P/Week 7 problems/test_watch.py
+++ CS50P/Week 7 problems/test_watch.py
@@ -1,18 +1,18 @@
 from watch import parse
 
 def test_parse():
-    assert parse("<iframe width="560" height="315" src="https://www.youtube.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>") == "https://youtu.be/xvFZjo5PgG0"
+    assert parse('"<iframe width="560" height="315" src="https://www.youtube.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>"') == "https://youtu.be/xvFZjo5PgG0"
     
-    assert parse("<iframe width="560" height="315" src="https://youtube.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>") == "https://youtu.be/xvFZjo5PgG0"
+    assert parse('"<iframe width="560" height="315" src="https://youtube.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>"') == "https://youtu.be/xvFZjo5PgG0"
     
-    assert parse("<iframe width="560" height="315" src="https://www.youtube.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>") == "https://youtu.be/xvFZjo5PgG0"
+    assert parse('"<iframe width="560" height="315" src="https://www.youtube.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>"') == "https://youtu.be/xvFZjo5PgG0"
     
     
 def wrong():
-    assert parse("<iframe width="560" height="315" src="https://www.twitter.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>") == None
+    assert parse('"<iframe width="560" height="315" src="https://www.twitter.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>"') == None
     
     assert parse("1") == None
     
     assert parse("") == None
     
-    assert parse("1234441234 asdfax") = None
+    assert parse("1234441234 asdfax") == None
--- CS50P/Week 7 problems/watch.py
+++ CS50P/Week 7 problems/watch.py
@@ -2,12 +2,15 @@ import re
 import sys
 
 
+#im not greedy https://stackoverflow.com/questions/2503413/regular-expression-to-stop-at-first-match
+
 def main():
     print(parse(input("HTML: ")))
 
 
 def parse(s):
-    if matches := re.search(r'"src=https?://(www.)?youtube\.com/embed/xvFZjo5PgG0"', s)
+    if matches := re.search(r'src="https?://(?:www.)?youtube\.com/embed/(.*?)"', s):
+        return f"https://youtu.be/{matches.group(1)}"
     else:
         return None
 
