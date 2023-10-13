5725ed1b6eed30f577a3b9682fe9fd34d0aa040d
Aremeer <theartemeer@gmail.com>
2023-10-13 13:48:01 +0200

    1

--- CS50P/Week 8/students.py
+++ CS50P/Week 8/students.py
@@ -1,7 +1,5 @@
 class Student:
     def __init__(self, name, house):
-        if not name:
-            raise ValueError("Missing Name")
         self.house = house
         self.name = name
 
@@ -9,18 +7,22 @@ class Student:
         return f"{self.name} from {self.house}"
 
     @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing Name")
+        self._name = name
+
+    @property
     def house(self):
         return self._house
-
     @house.setter 
     def house(self, house):
         if house not in ["Gryffindor","Hufflepuff","Ravenclaw","Slytherin"]:
             raise ValueError("invalid house")
         self._house = house
-
-
-
-
 
 
 def main():
