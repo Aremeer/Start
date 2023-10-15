dbccc01edc626baff4f99ea64c037e0a8cc4c58a
Aremeer <theartemeer@gmail.com>
2023-10-15 20:03:10 +0200

    1

--- CS50P/Week 8 problems/seasons_fail1.py
+++ CS50P/Week 8 problems/seasons_fail1.py
@@ -3,33 +3,29 @@ import re
 
 class DateOfBirth:
     def __init__(self, date_of_birth):
-        self.date = date_of_birth
-        if matches := re.match(r"^(\d{4})-(\d{2})-(\d{2})$", date_of_birth):
-            year, month, day = matches.group(1,2,3)
-            if year > str(date.today())[0:3]: exit("Invalid year")
-            if int(month) > 12: exit("Invalid month")
-            if int(day) > 31: exit("invalid day")
+        self.date_of_birth = date_of_birth
+        if matches := re.match(r"^\d{4}-\d{2}-\d{2}$", self.date_of_birth):
+            year, month, day = [date.fromisoformat(date_of_birth)]
             self.year = year
             self.month = month
             self.day = day
         else: exit("Invalid input")
+
+        def changeinto():
+            
 
     def __str__(self):
         return f"{self.year}-{self.month}-{self.day}"
 
     @classmethod
     def get(cls):
-        date = input("Date of birth: ")
-        return cls(date)
-
-    @classmethod
-    def into_minutes():
-        ...
+        date_of_birth = input("Date of birth: ")
+        return cls(date_of_birth)
 
 
 def main():
     date = (DateOfBirth.get())
-    print(DateOfBirth.into_minutes())
+    print(date)
 
 
 
