f0fcd8d4b97bc4ebdbe1f48e3f72fa0fdd5663b1
Aremeer <theartemeer@gmail.com>
2023-10-17 15:44:51 +0200

    1

--- CS50P/Final Project/Sudoku/simple_login_GUI.py
+++ CS50P/Final Project/Sudoku/simple_login_GUI.py
@@ -1,36 +1,15 @@
 """"GUI that will be a white backgroud a big box in the top with sudoku appearance, and two buttons at the bottom - randomise, and solve"""
 
 import customtkinter
+from sudoku import get_sudoku_value
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
 
-class MyFrame(customtkinter.CTkFrame):
-    def __init__(self, master, **kwargs):
-        super().__init__(master, **kwargs)
-        
-        # add widgets onto the frame, for example:
-        self.frame = customtkinter.CTkFrame(master=master, width=180, height=180, border_width=2)
-        self.frame.grid(row=0, column=0, padx=2, pady=2)
-
-
-class App(customtkinter.CTk):
-    def __init__(self):
-        super().__init__()
-        self.geometry("800x600")
-        self.grid_rowconfigure(2, weight=1)  # configure grid system
-        self.grid_columnconfigure(2, weight=1)
-        
-        self.my_frame = MyFrame(master=self)
-        self.my_frame.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
-        self.my_frame1 = MyFrame(master=self)
-        self.my_frame1.grid(row=0, column=1, padx=20, pady=20, sticky="nsew")
 
 
 
-app = App()
-app.mainloop()
 
 
 
---
+++ CS50P/Final Project/Sudoku/sudoku.py
@@ -0,0 +1,52 @@
+#https://docs.python.org/3/library/csv.html
+#https://www.w3schools.com/python/default.asp
+
+
+
+import csv
+import random
+
+
+
+"""so the idea is that you would create a csv formated list of lists where you have 9 rows.
+    key value parirs of keys[1-9]:list[1-9]"""
+
+def main():
+    get_sudoku_csv()
+    get_sudoku_value()
+
+def get_sudoku_value():
+    sudoku_list = []
+    with open("sudoku.csv", newline="") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            sudoku_list.append(row)
+        return sudoku_list
+def get_sudoku_csv():
+    with open("sudoku.csv", "w", newline="") as file:
+        fieldnames = ["1","2","3","4","5","6","7","8","9"]
+        writer= csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        for _ in range(9):
+            numbers: dict = {}
+            range_of_randoms: list = [1,2,3,4,5,6,7,8,9]
+            for n in range(9):
+                random_number: int = random.choice(range_of_randoms)
+                numbers.update({f"{n+1}":f"{random_number}"})
+                range_of_randoms.remove(random_number)
+                n +=1
+            range_of_randoms: list = [1,2,3,4,5,6,7,8,9]
+            choesn_numbers: list = []
+            for _ in range(random.randint(3,5)):
+                random_number: int = random.choice(range_of_randoms)
+                choesn_numbers.append(random_number)
+            for n in range(len(choesn_numbers)):
+                key_to_delete: int = choesn_numbers[n]
+                numbers.update({f"{key_to_delete}":""})
+            writer.writerow(numbers)
+
+
+
+
+if __name__ == "__main__":
+    main()
