3200cdc91c026e7437bcd795ec348cd0494d7b4a
Aremeer <theartemeer@gmail.com>
2023-10-18 07:10:11 +0200

    1

--- CS50P/Final Project/Sudoku/1.py
+++ CS50P/Final Project/Sudoku/1.py
@@ -11,56 +11,68 @@
 import customtkinter as ctk
 from sudoku import get_sudoku_csv
 import csv
-import random
+import time
 
-class MySmallFrame(ctk.CTkFrame):
-    def __init__(self, master, numbers):
-        super().__init__(master)
-        for c in range(9):
-            number_list = numbers[c]
-            for r in range(9):
-                number = number_list[r]
-                self.textbox = ctk.CTkTextbox(master=self, height=54, width=54)
-                self.textbox.grid(row=r, column=c, padx=2, pady=2, sticky="nswe")
-                self.textbox.insert("0.0", number)
-        
-        
 
 
 class MyFramesFrame(ctk.CTkFrame):
     def __init__(self, master, numbers):
         super().__init__(master)
-        self.grid_rowconfigure(3, weight=1)
-        self.grid_columnconfigure(3, weight=1)
-        
-        self.small_frame = MySmallFrame(self, numbers)
-        self.small_frame.configure(border_width=4, border_color="#FFFFFF")
-        self.small_frame.grid(row=0, column=0, padx=0, pady=0, sticky="nwse")
-
+        c=0
+        r=0
+        rr=0
+        for n in range(9):
+            if numbers[n] == "0":
+                number = ""
+            else: number = numbers[n]
+            self.textbox = ctk.CTkTextbox(master=self, height=54, width=54)
+            self.textbox.grid(row=r, column=c, padx=2, pady=2, sticky="nswe")
+            self.textbox.insert("0.0", number)
+            if c==2:
+                c=0 
+            else:
+                c+=1
+            if rr==2: 
+                r+=1
+                rr=0
+            else:
+                rr+=1
 
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
         numbers = []
-        with open("sudoku.csv", newline="") as file:
+        with open("api.csv", newline="") as file:
             reader = csv.reader(file)
             for row in reader:
                 numbers.append(row)
         self.title("Sudoku")
         self.geometry("600x700")
-        self.grid_columnconfigure(1, weight=1)
-        self.grid_rowconfigure(2, weight=1)
+        self.grid_columnconfigure(3, weight=1)
+        self.grid_rowconfigure(4, weight=1)
         
         self.button = ctk.CTkButton(self, text="New Game")
-        self.button.grid(row=1, column=0, padx=10, pady=10, sticky="nw", columnspan=1)
+        self.button.grid(row=3, column=0, padx=10, pady=10, sticky="nw", columnspan=1)
         
         self.button = ctk.CTkButton(self, text="Solve")
-        self.button.grid(row=1, column=0, padx=10, pady=10, sticky="ne", columnspan=1)
-
-        self.frame = MyFramesFrame(self, numbers)
-        self.frame.configure()
-        self.frame.grid(row=0, column=0, padx=10, pady=10, sticky="nwse")
-
+        self.button.grid(row=3, column=2, padx=10, pady=10, sticky="ne", columnspan=1)
+        
+        c=0
+        r=0
+        rr=0
+        for n in range(9):
+            self.frame = MyFramesFrame(self, numbers[n])
+            self.frame.configure(border_width=4, border_color="#FFFFFF")
+            self.frame.grid(row=r, column=c, padx=10, pady=10, sticky="nwse")
+            if c==2:
+                c=0 
+            else:
+                c+=1
+            if rr==2: 
+                r+=1
+                rr=0
+            else:
+                rr+=1
 
 app = App()
 app.mainloop()
--- CS50P/Final Project/Sudoku/API_sudoku.py
+++ CS50P/Final Project/Sudoku/API_sudoku.py
@@ -1,3 +1,6 @@
+#https://www.w3schools.com/python/python_json.asp
+#https://www.w3schools.com/python/ref_requests_response.asp
+
 import csv
 import json
 import requests
@@ -7,10 +10,12 @@ import requests
 
 def main():
     sudoku = requests.get("https://sudoku-api.vercel.app/api/dosuku?query={newboard(limit:1){grids{value}}}")
-    print(json.dumps)
-    (sudoku.json())
-    print(sudoku)
-
+    sudoku = sudoku.json()
+    sudoku: list = sudoku["newboard"]["grids"][0]["value"]
+    with open("api.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        for row in sudoku:
+            writer.writerow(row)
 
 
 
