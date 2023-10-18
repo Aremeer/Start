b7e5b729b680690a36bcfdb271fc51fb613152e4
Aremeer <theartemeer@gmail.com>
2023-10-18 23:33:40 +0200

    1

--- CS50P/Final Project/Sudoku/API_sudoku.py
+++ CS50P/Final Project/Sudoku/API_sudoku.py
@@ -8,11 +8,10 @@ import requests
 
 def main():
     sudoku_x, sudoku_y = read()
-    print(sudoku_x, "\n")
-    print(sudoku_y, "\n")
+    box = []
     for y in range(9):
         for x in range(9):
-            boolean = check(sudoku_x, sudoku_y, y, x)
+            boolean, box = check(sudoku_x, sudoku_y, y, x, box)
             print(boolean)
 
 def get():
@@ -43,32 +42,102 @@ def read():
             sudoku_y.append(row)
         return sudoku_x, sudoku_y
 
-def check(sudoku_x, sudoku_y, y, x):
+def check(sudoku_x, sudoku_y, y, x, box):
     reference = [1,2,3,4,5,6,7,8,9]
-    if y in [0,1,2] and x in [0,1,2]:
+    if y in [0] and x in [0]:
         box = []
         lis = sudoku_x[0]
         box.extend(lis[0:3])
         lis = sudoku_x[1]
         box.extend(lis[0:3])
-        lis = sudoku_x[1]
+        lis = sudoku_x[2]
         box.extend(lis[0:3])
     
+    if y in [0] and x in [3]:
+        box = []
+        lis = sudoku_x[3]
+        box.extend(lis[3:6])
+        lis = sudoku_x[4]
+        box.extend(lis[3:6])
+        lis = sudoku_x[5]
+        box.extend(lis[3:6])
     
+    if y in [0] and x in [6]:
+        box = []
+        lis = sudoku_x[6]
+        box.extend(lis[6:9])
+        lis = sudoku_x[7]
+        box.extend(lis[6:9])
+        lis = sudoku_x[8]
+        box.extend(lis[6:9])
+    
+    if y in [3] and x in [0]:
+        box = []
+        lis = sudoku_x[0]
+        box.extend(lis[0:3])
+        lis = sudoku_x[1]
+        box.extend(lis[0:3])
+        lis = sudoku_x[2]
+        box.extend(lis[0:3])
+    
+    if y in [3] and x in [3]:
+        box = []
+        lis = sudoku_x[3]
+        box.extend(lis[3:6])
+        lis = sudoku_x[4]
+        box.extend(lis[3:6])
+        lis = sudoku_x[5]
+        box.extend(lis[3:6])
+    
+    if y in [3] and x in [6]:
+        box = []
+        lis = sudoku_x[6]
+        box.extend(lis[6:9])
+        lis = sudoku_x[7]
+        box.extend(lis[6:9])
+        lis = sudoku_x[8]
+        box.extend(lis[6:9])
+    
+    if y in [6] and x in [0]:
+        box = []
+        lis = sudoku_x[0]
+        box.extend(lis[6:9])
+        lis = sudoku_x[1]
+        box.extend(lis[6:9])
+        lis = sudoku_x[2]
+        box.extend(lis[6:9])
+    
+    if y in [6] and x in [3]:
+        box = []
+        lis = sudoku_x[3]
+        box.extend(lis[6:9])
+        lis = sudoku_x[4]
+        box.extend(lis[6:9])
+        lis = sudoku_x[5]
+        box.extend(lis[6:9])
+    
+    if y in [6] and x in [6]:
+        box = []
+        lis = sudoku_x[6]
+        box.extend(lis[6:9])
+        lis = sudoku_x[7]
+        box.extend(lis[6:9])
+        lis = sudoku_x[8]
+        box.extend(lis[6:9])
 
-    box.sort()
-    result_y = sudoku_y[y]
+
+    sorted_box = []
+    sorted_box.extend(box)
+    sorted_box.sort()
+    result_y = []
+    result_y.extend(sudoku_x[x])
     result_y.sort()
-    result_x = sudoku_x[x]
+    result_x = []
+    result_x.extend(sudoku_y[y])
     result_x.sort()
-    print(sudoku_x, "\n")
-    print(sudoku_y, "\n")
-    print(box, "\n")
-    print(result_y, "\n")
-    print(result_x, "\n")
-    if box == reference and result_y == reference and result_x == reference:
-        return True
-    else: return False
+    if sorted_box == reference and result_y == reference and result_x == reference:
+        return True, box
+    else: return False, box
 
 
 
