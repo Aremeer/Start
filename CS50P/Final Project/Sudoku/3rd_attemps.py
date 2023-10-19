27db86c4c28740e5921844e6e6cf6bb1ce19a805
Aremeer <theartemeer@gmail.com>
2023-10-19 07:11:13 +0200

    1

--- CS50P/Final Project/Sudoku/3rd_attemps.py
+++ CS50P/Final Project/Sudoku/3rd_attemps.py
@@ -6,8 +6,8 @@ import random
 
 
 
-#calculate strting-potential
-#set current-potentioal = starting-potential
+
+#set current-potentioal
 #try first CP and expand
 #move next, recalc CP and use one
 #repeat until no CP left if so backtrack and try again
@@ -19,26 +19,77 @@ import random
 
 def main():
     sudoku_x = read()
-    box_i = 0
-    boxes = []
+    sudokus = get(sudoku_x)
+    
+    while True:
+        current_index = 0
+        
+        box = []
+        for r in range(9):
+            box_value = sudokus[current_index]["box"]
+            boxes = []
+            for i, value in enumerate(sudokus):
+                key = value["box"]
+                if key == box_value:
+                    boxes.append(i)
+            print(boxes)
+            break
+        
+        
+        
+        sorted_box_l = []
+        sorted_box_l.extend(box)
+        sorted_box_l.sort()
+        result_y_l = []
+        result_y_l.extend(sudoku_x[x])
+        result_y_l.sort()
+        result_x_l = []
+        result_x_l.extend(sudoku_y[y])
+        result_x_l.sort()
+        reference = [0,1,2,3,4,5,6,7,8,9]
+        difference = list(set(reference)-set(sorted_box_l))
+        difference = list(set(difference)-set(result_y_l))
+        difference = list(set(difference)-set(result_y_l))
+
+
+
+
+
+
+
+
+
+def get(sudoku_x):
+    sudoku_x = read()
+    sudokus = []
     index = 0
+    count = 0
+    box_row = 0
     for y in range(9):
-        count = 0
+        if box_row == 0 or box_row == 1 or box_row == 2:
+            box_i = 0
+        if box_row == 3 or box_row == 4 or box_row == 5:
+            box_i = 3
+        if box_row == 6 or box_row == 7 or box_row == 8:
+            box_i = 6
+        box_row += 1
         for x in range(9):
-            if count == 2:
-                count = 0
-                box_i += 1
             box = {
                 "index":index,
                 "value":sudoku_x[y][x],
                 "row":y,
                 "column":x,
                 "box":box_i,
-                "starting_potential":[],
-                "cuurent_pontetial":[]
+                "pontetial":[]
                 }
+            if count == 2:
+                count = 0
+                box_i += 1
+            else:
+                count += 1
             index += 1
-            boxes.append(box)
+            sudokus.append(box)
+    return sudokus
 
 def read():
     sudoku_x = []
