a0af479ef9853ef0072c7dcac08d0d6455ce1628
Aremeer <theartemeer@gmail.com>
2023-10-20 15:36:34 +0200

    1

--- CS50P/Final Project/project/test_project.py
+++ CS50P/Final Project/project/test_project.py
@@ -2,9 +2,18 @@ import csv
 import requests
 import json
 
-def read1():
+test1 = "test1_sudoku.csv"
+test11 = "test1_solution.csv"
+test2 = "test2_sudoku.csv"
+test22 = "test2_solution.csv"
+test3 = "test3_sudoku.csv"
+test33 = "test3_solution.csv"
+
+
+
+def read1(s):
     sudoku = []
-    with open("test1_sudoku.csv", newline="") as file:
+    with open(s, newline="") as file:
         reader = csv.reader(file)
         for row in reader:
             new = []
@@ -14,57 +23,9 @@ def read1():
             sudoku.append(new)
     return sudoku
 
-def read1_answer():
+def read1_answer(s):
     answer = []
-    with open("test1_solution.csv", newline="") as file:
-        reader = csv.reader(file)
-        for row in reader:
-            new = []
-            for dig in row:
-                dig = int(dig)
-                new.append(dig)
-            answer.append(new)
-    return answer
-
-def read2():
-    sudoku2 = []
-    with open("test2_sudoku.csv", newline="") as file:
-        reader = csv.reader(file)
-        for row in reader:
-            new = []
-            for dig in row:
-                dig = int(dig)
-                new.append(dig)
-            sudoku.append(new)
-    return sudoku2
-
-def read2_answer():
-    answer = []
-    with open("test2_solution.csv", newline="") as file:
-        reader = csv.reader(file)
-        for row in reader:
-            new = []
-            for dig in row:
-                dig = int(dig)
-                new.append(dig)
-            answer.append(new)
-    return answer
-
-def read3():
-    sudoku3 = []
-    with open("test3_sudoku.csv", newline="") as file:
-        reader = csv.reader(file)
-        for row in reader:
-            new = []
-            for dig in row:
-                dig = int(dig)
-                new.append(dig)
-            sudoku3.append(new)
-    return sudoku3
-
-def read3_answer():
-    answer = []
-    with open("test3_solution.csv", newline="") as file:
+    with open(s, newline="") as file:
         reader = csv.reader(file)
         for row in reader:
             new = []
