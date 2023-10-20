4e6ccda9445d2e4ff1edea6337b213f25aa03830
Aremeer <theartemeer@gmail.com>
2023-10-20 13:53:44 +0200

    1

--- CS50P/Final Project/project/test_project.py
+++ CS50P/Final Project/project/test_project.py
@@ -2,19 +2,85 @@ import csv
 import requests
 import json
 
+def read1():
+    sudoku = []
+    with open("test1_sudoku.csv", newline="") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            new = []
+            for dig in row:
+                dig = int(dig)
+                new.append(dig)
+            sudoku.append(new)
+    return sudoku
+
+def read1_answer():
+    answer = []
+    with open("test1_solution.csv", newline="") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            new = []
+            for dig in row:
+                dig = int(dig)
+                new.append(dig)
+            answer.append(new)
+    return answer
+
+def read2():
+    sudoku2 = []
+    with open("test2_sudoku.csv", newline="") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            new = []
+            for dig in row:
+                dig = int(dig)
+                new.append(dig)
+            sudoku.append(new)
+    return sudoku2
+
+def read2_answer():
+    answer = []
+    with open("test2_solution.csv", newline="") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            new = []
+            for dig in row:
+                dig = int(dig)
+                new.append(dig)
+            answer.append(new)
+    return answer
+
+def read3():
+    sudoku3 = []
+    with open("test3_sudoku.csv", newline="") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            new = []
+            for dig in row:
+                dig = int(dig)
+                new.append(dig)
+            sudoku3.append(new)
+    return sudoku3
+
+def read3_answer():
+    answer = []
+    with open("test3_solution.csv", newline="") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            new = []
+            for dig in row:
+                dig = int(dig)
+                new.append(dig)
+            answer.append(new)
+    return answer
 
 
-sudoku = requests.get("https://sudoku-api.vercel.app/api/dosuku?query={newboard(limit:5){grids{value,solution,difficulty},results,message}}")
-dosuku = sudoku.json()
-sudoku = sudoku.json()
-sudoku: list = sudoku["newboard"]["grids"][0]["value"]
-with open("test3_sudoku.csv", "w", newline="") as file:
-    writer = csv.writer(file)
-    for row in sudoku:
-        writer.writerow(row)
 
-dosuku: list = dosuku["newboard"]["grids"][1]["solution"]
-with open("test3_solution.csv", "w", newline="") as file:
-    writer = csv.writer(file)
-    for row in dosuku:
-        writer.writerow(row)
+
+
+
+
+
+
+
+
