cefc7ac5627c7f54aec72b4d618b75cc1ce5b41b
Aremeer <theartemeer@gmail.com>
2023-10-20 10:48:09 +0200

    1

--- CS50P/Final Project/Sudoku/3rd_attemps.py
+++ CS50P/Final Project/Sudoku/3rd_attemps.py
@@ -1,20 +1,6 @@
-
-
 import requests
 import csv
-import random
-import os 
-import time
-
-
-
-#set current-potentioal--------
-#try first CP and expand-----
-#move next, recalc CP and use one-----
-#repeat until no CP left if so backtrack and try again
-
-
-
+import os
 
 
 def main():
@@ -22,67 +8,72 @@ def main():
     sudoku_x = read()
     sudokus = get(sudoku_x)
     current_index = 0
+    os.system("cls")
+    printer(sudokus)
+    x = input("Do you want me to soleve the sudoku? [y/n]? ").rstrip(" ").lower()
+    if x == "y" or x == "yes":
+        pass
+    else: exit(0)
+    
     while True:
         if current_index > 80 or current_index < 0:
-                break
+            break
+        
         if sudokus[current_index]["solid"] == True:
             current_index+= 1
-            print(current_index, "is a solid")
             continue
+        
         if sudokus[current_index]["solid"] == False:
             difference = check(sudokus, current_index)
             if len(difference) == 1:
                 sudokus[current_index]["value"] = difference[0]
                 os.system("cls")
-                print(current_index,"diff", difference)
                 printer(sudokus)
                 current_index +=1
-                
                 continue
+            
             elif len(difference) > 1:
                 sudokus[current_index]["value"] = difference.pop(0)
                 sudokus[current_index]["potential"] = difference
                 os.system("cls")
-                print(current_index,"diff",  difference)
                 printer(sudokus)
                 current_index +=1
-                
                 continue
+            
             elif len(difference) == 0:
                 while True:
                     if current_index < 0 or current_index > 80:
                         break
+                    
                     elif sudokus[current_index]["solid"] == True:
                         current_index -= 1
                         os.system("cls")
-                        print(current_index, "is a solid")
                         printer(sudokus)
+                    
                     elif sudokus[current_index]["solid"] == False:
                         if sudokus[current_index]["potential"] == False:
                             sudokus[current_index]["value"] = 0
                             current_index -= 1
                             os.system("cls")
-                            print(current_index, "has no potential")
                             printer(sudokus)
-                            
                             continue
+                        
                         if len(sudokus[current_index]["potential"]) == 1:
                             sudokus[current_index]["value"]  = sudokus[current_index]["potential"][0]
                             sudokus[current_index]["potential"] = False
                             os.system("cls")
-                            print(current_index,"potential",  sudokus[current_index]["potential"])
                             printer(sudokus)
                             current_index +=1
-                            
                             break
+                        
                         if len(sudokus[current_index]["potential"]) > 1:
                             sudokus[current_index]["value"] = sudokus[current_index]["potential"].pop(0)
                             os.system("cls")
-                            print(current_index,"potential", sudokus[current_index]["potential"])
                             printer(sudokus)
                             current_index +=1
-                            
                             break
+                        
+    save_result(sudokus)
     os.system("cls")
     printer(sudokus)
 
@@ -158,8 +149,8 @@ def api():
     while True:
         sudoku = requests.get("https://sudoku-api.vercel.app/api/dosuku?query={newboard(limit:5){grids{value,solution,difficulty},results,message}}")
         sudoku = sudoku.json()
-        if sudoku["newboard"]["grids"][2]["difficulty"] != "Easy":
-            continue
+        """if sudoku["newboard"]["grids"][2]["difficulty"] != "Easy":
+            continue"""
         sudoku: list = sudoku["newboard"]["grids"][0]["value"]
         with open("api.csv", "w", newline="") as file:
             writer = csv.writer(file)
@@ -179,20 +170,30 @@ def read():
             sudoku_x.append(new)
     return sudoku_x
 
+def save_result(s):
+    with open("result.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        for y in range(9):
+            y += 1
+            row = []
+            for x in range(9):
+                row.append(s[y*x]["value"])
+            writer.writerow(row)
+
 def printer(s):
     print(
     f"""
-    {s[0]["value"]}{s[1]["value"]}{s[2]["value"]}|{s[3]["value"]}{s[4]["value"]}{s[5]["value"]}|{s[6]["value"]}{s[7]["value"]}{s[8]["value"]}
-    {s[9]["value"]}{s[10]["value"]}{s[11]["value"]}|{s[12]["value"]}{s[13]["value"]}{s[14]["value"]}|{s[15]["value"]}{s[16]["value"]}{s[17]["value"]}
-    {s[18]["value"]}{s[19]["value"]}{s[20]["value"]}|{s[21]["value"]}{s[22]["value"]}{s[23]["value"]}|{s[24]["value"]}{s[25]["value"]}{s[26]["value"]}
-    -----------
-    {s[27]["value"]}{s[28]["value"]}{s[29]["value"]}|{s[30]["value"]}{s[31]["value"]}{s[32]["value"]}|{s[33]["value"]}{s[34]["value"]}{s[35]["value"]}
-    {s[36]["value"]}{s[37]["value"]}{s[38]["value"]}|{s[39]["value"]}{s[40]["value"]}{s[41]["value"]}|{s[42]["value"]}{s[43]["value"]}{s[44]["value"]}
-    {s[45]["value"]}{s[46]["value"]}{s[47]["value"]}|{s[48]["value"]}{s[49]["value"]}{s[50]["value"]}|{s[51]["value"]}{s[52]["value"]}{s[53]["value"]}
-    -----------
-    {s[54]["value"]}{s[55]["value"]}{s[56]["value"]}|{s[57]["value"]}{s[58]["value"]}{s[59]["value"]}|{s[60]["value"]}{s[61]["value"]}{s[62]["value"]}
-    {s[63]["value"]}{s[64]["value"]}{s[65]["value"]}|{s[66]["value"]}{s[67]["value"]}{s[68]["value"]}|{s[69]["value"]}{s[70]["value"]}{s[71]["value"]}
-    {s[72]["value"]}{s[73]["value"]}{s[74]["value"]}|{s[75]["value"]}{s[76]["value"]}{s[77]["value"]}|{s[78]["value"]}{s[79]["value"]}{s[80]["value"]}
+    {s[0]["value"]} {s[1]["value"]} { s[2]["value"]} | {s[3]["value"]} {s[4]["value"]} {s[5]["value"]} | {s[6]["value"]} {s[7]["value"]} {s[8]["value"]}
+    {s[9]["value"]} { s[10]["value"]} {s[11]["value"]} | {s[12]["value"]} {s[13]["value"]} {s[14]["value"]} | {s[15]["value"]} {s[16]["value"]} {s[17]["value"]}
+    {s[18]["value"]} {s[19]["value"]} {s[20]["value"]} | {s[21]["value"]} {s[22]["value"]} {s[23]["value"]} | {s[24]["value"]} {s[25]["value"]} {s[26]["value"]}
+    ---------------------
+    {s[27]["value"]} {s[28]["value"]} {s[29]["value"]} | {s[30]["value"]} {s[31]["value"]} {s[32]["value"]} | {s[33]["value"]} {s[34]["value"]} {s[35]["value"]}
+    {s[36]["value"]} {s[37]["value"]} {s[38]["value"]} | {s[39]["value"]} {s[40]["value"]} {s[41]["value"]} | {s[42]["value"]} {s[43]["value"]} {s[44]["value"]}
+    {s[45]["value"]} {s[46]["value"]} {s[47]["value"]} | {s[48]["value"]} {s[49]["value"]} {s[50]["value"]} | {s[51]["value"]} {s[52]["value"]} {s[53]["value"]}
+    ---------------------
+    {s[54]["value"]} {s[55]["value"]} {s[56]["value"]} | {s[57]["value"]} {s[58]["value"]} {s[59]["value"]} | {s[60]["value"]} {s[61]["value"]} {s[62]["value"]}
+    {s[63]["value"]} {s[64]["value"]} {s[65]["value"]} | {s[66]["value"]} {s[67]["value"]} {s[68]["value"]} | {s[69]["value"]} {s[70]["value"]} {s[71]["value"]}
+    {s[72]["value"]} {s[73]["value"]} {s[74]["value"]} | {s[75]["value"]} {s[76]["value"]} {s[77]["value"]} | {s[78]["value"]} {s[79]["value"]} {s[80]["value"]}
     """
     )
 
