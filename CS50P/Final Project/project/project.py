0542f4eb29882a3295d264a1f7173bd54d09de4b
Aremeer <theartemeer@gmail.com>
2023-10-21 11:56:31 +0200

    1

--- CS50P/Final Project/project/project.py
+++ CS50P/Final Project/project/project.py
@@ -2,63 +2,67 @@ import requests
 import csv
 import os
 
+
 def main():
     api()
     sudoku_x = read("api.csv")
     sudokus = get_dict(sudoku_x)
     printer(sudokus)
-    
+
     x = input("Do you want me to solve the sudoku? [y/n]? ").rstrip(" ").lower()
     if x == "y" or x == "yes":
         pass
-    else: exit(0)
+    else:
+        exit(0)
     os.system("cls")
-    
-    sudokus = (check_values(sudokus))
+
+    sudokus = check_values(sudokus)
     save_result(sudokus)
     os.system("cls")
     printer(sudokus)
+
 
 def check_values(sudokus):
     current_index = 0
     while True:
         if current_index > 80 or current_index < 0:
             return sudokus
-        
+
         if sudokus[current_index]["solid"] == True:
-            current_index+= 1
+            current_index += 1
             continue
-        
+
         if sudokus[current_index]["solid"] == False:
             difference = check(sudokus, current_index)
             if len(difference) == 1:
                 sudokus[current_index]["value"] = difference[0]
                 os.system("cls")
                 printer(sudokus)
-                current_index +=1
+                current_index += 1
                 continue
-            
+
             elif len(difference) > 1:
                 sudokus[current_index]["value"] = difference.pop(0)
                 sudokus[current_index]["potential"] = difference
                 os.system("cls")
                 printer(sudokus)
-                current_index +=1
+                current_index += 1
                 continue
-            
+
             elif len(difference) == 0:
                 current_index, sudokus = check_negative(current_index, sudokus)
+
 
 def check_negative(current_index, sudokus):
     while True:
         if current_index < 0 or current_index > 80:
             return current_index, sudokus
-        
+
         elif sudokus[current_index]["solid"] == True:
             current_index -= 1
             os.system("cls")
             printer(sudokus)
-        
+
         elif sudokus[current_index]["solid"] == False:
             if sudokus[current_index]["potential"] == False:
                 sudokus[current_index]["value"] = 0
@@ -66,52 +70,56 @@ def check_negative(current_index, sudokus):
                 os.system("cls")
                 printer(sudokus)
                 continue
-            
+
             if len(sudokus[current_index]["potential"]) == 1:
-                sudokus[current_index]["value"]  = sudokus[current_index]["potential"][0]
+                sudokus[current_index]["value"] = sudokus[current_index]["potential"][0]
                 sudokus[current_index]["potential"] = False
                 os.system("cls")
                 printer(sudokus)
-                current_index +=1
-                return current_index, sudokus
-            
-            if len(sudokus[current_index]["potential"]) > 1:
-                sudokus[current_index]["value"] = sudokus[current_index]["potential"].pop(0)
-                os.system("cls")
-                printer(sudokus)
-                current_index +=1
+                current_index += 1
                 return current_index, sudokus
 
+            if len(sudokus[current_index]["potential"]) > 1:
+                sudokus[current_index]["value"] = sudokus[current_index][
+                    "potential"
+                ].pop(0)
+                os.system("cls")
+                printer(sudokus)
+                current_index += 1
+                return current_index, sudokus
+
+
 def check(sudokus, current_index):
-        box = []
-        box_value = sudokus[current_index]["box"]
-        for dict in sudokus:
-            key = dict["box"]
-            if key == box_value:
-                box.append(int(dict["value"]))
-        
-        row=[]
-        row_value = sudokus[current_index]["row"]
-        for dict in sudokus:
-            key = dict["row"]
-            if key == row_value:
-                row.append(int(dict["value"]))
-        
-        column = []
-        column_value = sudokus[current_index]["column"]
-        for dict in sudokus:
-            key = dict["column"]
-            if key == column_value:
-                column.append(int(dict["value"]))
-        
-        box.sort()
-        row.sort()
-        column.sort()
-        reference = [0,1,2,3,4,5,6,7,8,9]
-        diff = list(set(reference)-set(box))
-        diff = list(set(diff)-set(row))
-        diff = list(set(diff)-set(column))
-        return diff
+    box = []
+    box_value = sudokus[current_index]["box"]
+    for dict in sudokus:
+        key = dict["box"]
+        if key == box_value:
+            box.append(int(dict["value"]))
+
+    row = []
+    row_value = sudokus[current_index]["row"]
+    for dict in sudokus:
+        key = dict["row"]
+        if key == row_value:
+            row.append(int(dict["value"]))
+
+    column = []
+    column_value = sudokus[current_index]["column"]
+    for dict in sudokus:
+        key = dict["column"]
+        if key == column_value:
+            column.append(int(dict["value"]))
+
+    box.sort()
+    row.sort()
+    column.sort()
+    reference = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    diff = list(set(reference) - set(box))
+    diff = list(set(diff) - set(row))
+    diff = list(set(diff) - set(column))
+    return diff
+
 
 def get_dict(sudoku_x):
     sudokus = []
@@ -128,14 +136,14 @@ def get_dict(sudoku_x):
         box_row += 1
         for x in range(9):
             box = {
-                "index":index,
-                "value":sudoku_x[y][x],
-                "row":y,
-                "column":x,
-                "box":box_i,
-                "potential":False,
-                "solid":True
-                }
+                "index": index,
+                "value": sudoku_x[y][x],
+                "row": y,
+                "column": x,
+                "box": box_i,
+                "potential": False,
+                "solid": True,
+            }
             if count == 2:
                 count = 0
                 box_i += 1
@@ -146,6 +154,7 @@ def get_dict(sudoku_x):
                 box["solid"] = False
             sudokus.append(box)
     return sudokus
+
 
 def api():
     while True:
@@ -161,6 +170,7 @@ def api():
                 writer.writerow(row)
         return writer
 
+
 def read(file):
     sudoku_x = []
     with open(file, newline="") as file:
@@ -173,6 +183,7 @@ def read(file):
             sudoku_x.append(new)
     return sudoku_x
 
+
 def save_result(s):
     with open("result.csv", "w", newline="") as file:
         writer = csv.writer(file)
@@ -180,12 +191,13 @@ def save_result(s):
             y += 1
             row = []
             for x in range(9):
-                row.append(s[y*x]["value"])
+                row.append(s[y * x]["value"])
             writer.writerow(row)
+
 
 def printer(s):
     print(
-    f"""
+        f"""
     {s[0]["value"]} {s[1]["value"]} { s[2]["value"]} | {s[3]["value"]} {s[4]["value"]} {s[5]["value"]} | {s[6]["value"]} {s[7]["value"]} {s[8]["value"]}
     {s[9]["value"]} { s[10]["value"]} {s[11]["value"]} | {s[12]["value"]} {s[13]["value"]} {s[14]["value"]} | {s[15]["value"]} {s[16]["value"]} {s[17]["value"]}
     {s[18]["value"]} {s[19]["value"]} {s[20]["value"]} | {s[21]["value"]} {s[22]["value"]} {s[23]["value"]} | {s[24]["value"]} {s[25]["value"]} {s[26]["value"]}
@@ -200,16 +212,6 @@ def printer(s):
     """
     )
 
+
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
